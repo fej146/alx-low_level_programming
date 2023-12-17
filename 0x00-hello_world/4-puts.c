f03d35490d6cf2460eebf839b30583011a677422
fej146 <eshunjnr@gmail.com>
2023-12-17 13:44:13 -0800

    printf function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /*
  * main - uses puts; not printf
  * Returns: 0
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - uses printf function
+ * Return: void (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
