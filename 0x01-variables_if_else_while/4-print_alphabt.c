17b50189b43862bf01df5aff49d7b249bd0b11ed
fej146 <eshunjnr@gmail.com>
2023-12-20 23:56:30 -0800

    without q&e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the entry point for the function
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e'&& c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
