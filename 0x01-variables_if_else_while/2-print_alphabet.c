821d5f16928b03d8ef53e3c06e07c7f65d86e616
fej146 <eshunjnr@gmail.com>
2023-12-20 22:53:41 -0800

    putchar small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the entry point for the function
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
