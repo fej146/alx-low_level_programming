1e38abf711ac301664b36a8851e4f67083e0bf49
fej146 <eshunjnr@gmail.com>
2023-12-21 21:24:29 -0800

    i is super-hope to be correct

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
-}
-
-#include "main.h"
 
 /**
  * _isupper - uppercase letters
