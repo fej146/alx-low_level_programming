379b383c1546c4dce419ec43d551435bf5ec1da4
fej146 <eshunjnr@gmail.com>
2023-12-23 11:20:36 -0800

    to be sorted out later

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Point of Entry for the code
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (a == '0'; a <= '9'; a++)
+			{
+				if (!(a == b || b == c || b > a || c > b)i))
+				{
+					putchar(c);
+					putchar(b);
+					putchar(a);
+					if (!(a == '9' && b == '8' && c == '7'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
