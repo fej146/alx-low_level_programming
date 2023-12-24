1cd09faca4fb2b04fd622c99d7c6bc585089f40a
fej146 <eshunjnr@gmail.com>
2023-12-24 13:58:54 -0800

    print the sign of the number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,17 +6,17 @@
  */
 int print_sign(int n)
 {
-	for (n > 0)
+	if (n > 0)
 	{
 		return (1);
 		_putchar('+');
 	}
-	for (n = 0)
+	else if (n == 0)
 	{
 		return (0);
 		_putchar('0');
 	}
-	for
+	else
 	{
 		return (-1);
 		_putchar('-');
