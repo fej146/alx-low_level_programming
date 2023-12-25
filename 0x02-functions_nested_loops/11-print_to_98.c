5d88844c0bf024052eb2b5299ba96273a87d105b
fej146 <eshunjnr@gmail.com>
2023-12-25 10:04:26 -0800

    prints natural numbers up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,13 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-		{
 			printf("%d, ", n--);
-		}
 		printf("%d\n", n);
 	}
 	else (n < 98)
 	{
 		while (n < 98)
-		{
 			printf("%d, ", n++);
-		}
 		printf("%d\n", n);
 	}
 }
