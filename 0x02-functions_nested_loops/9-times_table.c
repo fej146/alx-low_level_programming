44283faf4759417fad2e49920911089adfb1926d
fej146 <eshunjnr@gmail.com>
2023-12-25 05:23:27 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,7 @@ void times_table(void)
 			if (multiply == 0 && multiply < 10)
 			{
 				_putchar(multiply + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+
 			}
 			else if (multiply >= 10)
 			{
