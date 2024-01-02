7e433850ae4137df7d08333c020f6257cc31af37
fej146 <eshunjnr@gmail.com>
2024-01-02 14:23:34 -0800

    counts the number of char in a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - counts the number of characters in a string
+ * @s: characters to be counted
+ * Retunr: int of number of count
+ */
+int _strlen(char *s)
+{
+	int n;
+	int strl = 0; /*strl - string length*/
+
+	for (n = 0; s[n] != \0; n++)
+		strl++;
+	return (strl)
+}
