6fc18f1c034fbe74d77fe4181df374e535dfb9ec
fej146 <eshunjnr@gmail.com>
2024-01-02 20:23:44 -0800

    print characters of string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints the  characters inputed
+ * @str: strings
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+		putchar(s[n]);
+	return (0);
+}
