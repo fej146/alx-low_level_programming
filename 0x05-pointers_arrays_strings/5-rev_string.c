b93d5a18a84ef3bacf3f2a0919c89cb4638cb520
fej146 <eshunjnr@gmail.com>
2024-01-02 21:58:52 -0800

    swapt string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverses strings
+ * @s: the string tha is entered
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int i;
+	int count;
+
+	for (i = 0; S[i] != '\0'; i++)
+		count++;
+	for (i = 0; i < count / 2; i++)
+	{
+		char j;
+
+		j = s[i];
+		s[i] = s[count - 1 - i];
+		s[count - 1 - i] = j;
+	}
+}
