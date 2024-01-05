0b039fb33677bf27c80557743411cd7b26000342
fej146 <eshunjnr@gmail.com>
2024-01-04 18:50:02 -0800

    contactenate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	char i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	j = 0
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
