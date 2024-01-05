af6c7f69cba013454a8e7b3641e4393f50daf932
fej146 <eshunjnr@gmail.com>
2024-01-04 21:51:10 -0800

    tasks 6 - 104

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - encodes numbers in a string
+ * @s: string to encode
+ * Return: address of s
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
