477ad2b15e8e106dd89ed1621f4f2c8e2171ec52
fej146 <eshunjnr@gmail.com>
2024-01-18 12:33:09 -0800

    creating a static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - print the absolute value
+ * @c: th parameter entered
+ * Return: absolute value
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
