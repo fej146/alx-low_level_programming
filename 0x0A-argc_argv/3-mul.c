1eca9ff189d66392ca82e17d59824f8cbaa58746
fej146 <eshunjnr@gmail.com>
2024-01-19 14:03:22 -0800

    multiply 2 arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,18 +8,18 @@
  */
 int main(int argc, char *argv[])
 {
-	/*check if the program received 2 arguments*/
+	int num1, num2;
+
+	/*checks if the program received 2 arguments*/
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	/*convert the arguments passed to integers*/
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 	/*prints the products of the 2 arguments*/
 	printf("%d\n", num1 * num2);
 	return (0);
 }
-
