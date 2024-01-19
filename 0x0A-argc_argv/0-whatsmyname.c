db7fe249148200350b4ddf9aae089801e1600e6f
fej146 <eshunjnr@gmail.com>
2024-01-19 11:03:25 -0800

    command line that prints name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the point of entry of the program
+ * @argc: count of the argument
+ * @argv: argument vector
+ * Return: 0
+ */
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
