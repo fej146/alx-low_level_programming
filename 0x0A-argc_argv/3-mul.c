e4d321b7b3db53e277eeec4bdff4876fc4cfeb0b
fej146 <eshunjnr@gmail.com>
2024-01-21 09:20:14 -0800

    multiply 2 arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,19 +6,20 @@
  * @argv: the argument vector
  * Return: int
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int x, y;
+	int num1, num2;
 
+	/*checks if the program received 2 arguments*/
 	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	printf("%d\n", x * y);
-
+	/*convert the arguments passed to integers*/
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	/*prints the products of the 2 arguments*/
+	printf("%d\n", num1 * num2);
 	return (0);
 }
