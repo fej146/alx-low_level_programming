6b93326303c0018fda00992d8cd1361159899d19
fej146 <eshunfrancisjunior@yahoo.com>
2024-02-01 08:59:08 -0800

    initializing struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,5 +10,10 @@ struct dog;
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d = {"Poppy", 3.5, "Bob"};
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
