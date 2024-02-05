d4438dcf76fc4335bb37eeba1af07a83c093f3cf
fej146 <eshunfrancisjunior@yahoo.com>
2024-02-05 15:43:59 -0800

    add to beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (ptr == NULL)
 		return (NULL);
 	ptr->n = n;
-	ptr->next = head;
-	head = ptr;
+	ptr->next = *head;
+	*head = ptr;
 	return (ptr);
 }
