c5c2b9b352ce2796fc1f32f2064b66ff860b73db
fej146 <eshunfrancisjunior@yahoo.com>
2024-02-05 14:31:58 -0800

    list nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: head of node linklist
+ * Return: the number of nodes created
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int i; /*for the count of the nodes*/
+	
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i)
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+More tasks on the singly linked lists
