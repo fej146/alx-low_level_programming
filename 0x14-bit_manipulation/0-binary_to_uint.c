f0abb65cda360c22e2cf8a212fd6f990bfd173d8
fej146 <eshunfrancisjunior@yahoo.com>
2024-02-09 17:36:53 -0800

    binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-	for (int i = 0; i != '0'; i++)
+	for (int i = 0; b[i] != '\0'; i++)
 	{
 		total <<= 1;
 		if (b[i] == '1')
