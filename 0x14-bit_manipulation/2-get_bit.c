d5672d9636075ad42481d001c5da3c3e94e906bd
fej146 <eshunfrancisjunior@yahoo.com>
2024-02-11 06:24:17 -0800

    bit value of index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
-	if (index > sizeof(size_t) * 8)
+	if (index > sizeof(unsigned int) * 8)
 		return (-1);
 	for (i = 0; i < index; i++)
 		n = n >> 1;
